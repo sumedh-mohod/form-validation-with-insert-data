ee3648d70d86d9b447748a8c685da645b1abb15e
SUMEDH MOHOD <sumedhmohod72@gmail.com>
2022-03-24 16:46:24 +0530

    updated

--- index.php
+++ index.php
@@ -96,19 +96,18 @@ if ($_POST) {
 <?php
 if ($_POST) {
 //get data from form
-$Name = $_POST['name'];
-$Email= $_POST['email'];
-$Phone= $_POST['phone'];
-// $state= $_POST['state'];
+$enquiry = $_POST['enquiry'];
+$name = $_POST['name'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
 $country= $_POST['country'];
-// $Message= $_POST['message'];
 $to = "sumedhmohod72@gmail.com";
-$Subject = "Mail From website";
-$txt ="Name = ". $Name . "\r\n  Email = " . $Email . "\r\n Country =" . $country . "\r\n Phone =" . $Phone . "\r\n subject name =" . $Subject;
+$subject = "Mail From website";
+$txt ="enquiry = ". $enquiry ."\r\n name = ". $name . "\r\n  email = " . $email . "\r\n country =" . $country . "\r\n phone =" . $phone . "\r\n subject name =" . $subject;
 $headers = "From: vivekdeshmukh1214@gmail.com" . "\r\n" .
 "CC: somebodyelse@example.com";
-if($Email!=NULL){
-    mail($to,$Subject,$txt,$headers);
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
 }
 ?>
